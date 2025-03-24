b532d45b88e682e91e5f0c07e1d673906d695c2b
suibhne-sweeney <suibhnesweeney@gmail.com>
2025-03-24 21:28:16 +0000

    set up MongoDB connection

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace App_Server.Controllers
+{
+    [ApiController]
+    [Route("api/posts")]
+    public class PostsController : ControllerBase
+    {
+        // Mongo DB Setup and Connection
+        private readonly IMongoCollection<Post> postCollection;
+        
+        public PostsController() 
+        {
+            DotNetEnv.Env.Load();
+            var connectionString = Environment.GetEnvironmentVariable("MONGO_URI");
+            var databaseName = "test";
+            var collectionName = "posts";
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+            postCollection = database.GetCollection<Post>(collectionName);
+        }
+        
+        [HttpGet("feed")]
+        public async Task<IActionResult> GetFeedPosts()
+        {
+            try
+            {
+                var posts = await postCollection.Find(new BsonDocument()).ToListAsync();
+                return Ok(posts);
+            }
+            catch (Exception e)
+            {
+                
+                return NotFound(e);
+            }
+        }
+    }
+}
